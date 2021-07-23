bf112dcd027f461e03d264dd8e3a607a078f0ba6
Ernesto Martínez García <git@ecomaikgolf.com>
2021-07-24 01:51:44 +0200

    lock kernel pages

--- kernel/kernel.cpp
+++ kernel/kernel.cpp
@@ -4,12 +4,18 @@
  * @author Ernesto Martínez García <me@ecomaikgolf.com>
  */
 
+#include "PFA.h"
 #include "bootargs.h"
 #include "cstr.h"
 #include "float.h"
 #include "framebuffer.h"
 #include "math.h"
 #include "renderer.h"
+#include <stddef.h>
+#include <stdint.h>
+
+extern uint64_t _kernel_start;
+extern uint64_t _kernel_end;
 
 /**
  * Kernel starting function
@@ -20,8 +26,14 @@ extern "C" void
 _start(BootArgs *args)
 {
     Renderer renderer(args->fb, args->font);
+    renderer.println("Kernel");
 
-    renderer.println("Hola esto es con bootargs");
+    PFA pfa(args->map);
+
+    uint64_t _kernel_size  = &_kernel_end - &_kernel_start;
+    uint64_t _kernel_pages = _kernel_size / UEFIMMap::page_size;
+
+    pfa.lock_pages(&_kernel_start, _kernel_pages);
 
     while (1) {
     }
