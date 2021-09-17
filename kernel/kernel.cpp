a3235dc2d50c8a528b934b88dd04e72be6fe25a4
Ernesto Martínez García <git@ecomaikgolf.com>
2021-09-17 17:41:04 +0200

    commented the kernel _start function

--- kernel/kernel.cpp
+++ kernel/kernel.cpp
@@ -22,48 +22,55 @@ extern uint64_t _kernel_end;
  * Kernel starting function
  * extern C to avoid C++ function mangling
  */
-extern "C" void
+extern "C" [[noreturn]] void
 _start(BootArgs *args)
 {
+	/* Main screen renderer */
     Renderer renderer(args->fb, args->font);
 
+	/* Global descriptor table */
     gdt_ptr gdt;
     gdt.size   = sizeof(table) - 1;
     gdt.offset = (uint64_t)&table;
     load_gdt(&gdt);
 
-    if (args->map == NULL)
-        return;
-
+	/* Memory allocator */
     allocator = PFA(args->map);
 
+	/* Get kernel size */
     uint64_t _kernel_size  = (size_t)&_kernel_end - (size_t)&_kernel_start;
     uint64_t _kernel_pages = _kernel_size / UEFIMMap::page_size + 1;
 
+	/* Lock kernel's memory */
     allocator.lock_pages(&_kernel_start, _kernel_pages);
 
+	/* Initialise the page table manager */
     PTM page_table;
 
+	/* Map virtual memory to physical memory (same address for the kernel) */
     for (uint64_t i = 0; i < UEFIMMap::get_memsize(args->map); i += UEFIMMap::page_size) {
         page_table.map(i, i);
     }
 
+	/* Lock screen's memory */
     uint64_t fbbase = (uint64_t)args->fb->base;
     uint64_t fbsize = (uint64_t)args->fb->buffer_size + UEFIMMap::page_size;
-
     allocator.lock_pages((void *)fbbase, fbsize / UEFIMMap::page_size + 1);
 
-    for (uint64_t i = fbbase; i < fbbase + fbsize; i += UEFIMMap::page_size) {
-        page_table.map(i, i);
-    }
+	/* Map screen's memory */
+	for (uint64_t i = fbbase; i < fbbase + fbsize; i += UEFIMMap::page_size) {
+		page_table.map(i, i);
+	}
 
+	/* Enable virtual addresses */
     asm("mov %0, %%cr3" : : "r"(page_table.get_PGDT()));
 
-    /** @todo Can be improved doing it with GOP operations in UEFI */
-    memset(args->fb->base, 0, args->fb->buffer_size);
+	/* Clean the screen */
+	memset(args->fb->base, 0, args->fb->buffer_size);
 
-    renderer.println("Print con memoria virtual");
+	renderer.println("Hello from the kernel.");
 
+	/* Shoudln't return */
     while (1) {
     }
 }
