82ca7371d3f372fcf9786c0cc70028d7d6848ce5
Ernesto Martínez García <git@ecomaikgolf.com>
2021-09-17 18:56:23 +0200

    abs function uses requirements now, imported type_traits

--- kernel/libc/math/abs.h
+++ kernel/libc/math/abs.h
@@ -1,13 +1,16 @@
+#include "libstdc++/type_traits.h"
+
 /**
  * absolute value function
  *
  * https://isocpp.org/wiki/faq/templates#separate-template-fn-defn-from-decl
  */
-template<typename T>
-T
+template <typename T>
+requires(is_integral<T>::value || is_floating_point<T>::value) 
+int
 abs(T number)
 {
-    if (number < 0)
-        return (number * -1);
-    return number;
+	if(number < 0)
+		return number * -1;
+	return number;
 }
