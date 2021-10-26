1c3d2e0e61d9016c6f8df2be680c399c0001612e
Ernesto Martínez García <git@ecomaikgolf.com>
2021-10-26 22:56:09 +0200

    added kernel global variables

---
+++ kernel/kernel.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "paging/PFA.h"
+#include "paging/PTM.h"
+#include "screen/fonts/psf1.h"
+#include "segmentation/gdt.h"
+#include <stdint.h>
+
+/**
+ * Size of the kernel
+ *
+ * Values set by the linker script
+ */
+extern uint64_t _kernel_start;
+extern uint64_t _kernel_end;
+
+namespace kernel {
+
+extern paging::allocator::PFA allocator;
+extern paging::translator::PTM translator;
+extern screen::psf1_renderer tty;
+extern segmentation::gdt_ptr gdt;
+
+} // namespace kernel
