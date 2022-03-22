14f520062d773ea4158387138801e4e1b7afe977
Ernesto Martínez García <git@ecomaikgolf.com>
2022-03-22 19:46:23 +0100

    bugfix, fast renderer working

--- kernel/bootstrap/startup.cpp
+++ kernel/bootstrap/startup.cpp
@@ -21,11 +21,13 @@ screen(stivale2_struct *st)
     auto *mod = (stivale2_struct_tag_modules *)stivale2_get_tag(st, STIVALE2_STRUCT_TAG_MODULES_ID);
 
     framebuffer frame;
-    frame.base        = (unsigned int *)fb->framebuffer_addr;
-    frame.buffer_size = fb->framebuffer_height * fb->framebuffer_pitch;
-    frame.ppscl       = (fb->framebuffer_pitch / sizeof(uint32_t));
-    frame.width       = fb->framebuffer_width;
-    frame.height      = fb->framebuffer_height;
+    frame.base  = (unsigned int *)fb->framebuffer_addr;
+    frame.ppscl = (fb->framebuffer_pitch / sizeof(uint32_t));
+    frame.width = fb->framebuffer_width;
+    frame.width -= frame.width % 8;
+    frame.height = fb->framebuffer_height;
+    frame.height -= frame.height % 16;
+    frame.buffer_size = frame.height * frame.ppscl * sizeof(uint32_t);
 
     fonts::specification::psf1 *font_ptr =
       (fonts::specification::psf1 *)stivale2_get_mod(mod, "font");
