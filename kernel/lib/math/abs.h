3f72f51dd2ab8d36acdb952499e4de5ff69c44a2
Ernesto Martínez García <git@ecomaikgolf.com>
2022-04-15 03:31:59 +0200

    removed type traits

--- kernel/lib/math/abs.h
+++ kernel/lib/math/abs.h
@@ -1,12 +1,11 @@
-#include "lib/type_traits.h"
-
 /**
  * absolute value function
  *
  * https://isocpp.org/wiki/faq/templates#separate-template-fn-defn-from-decl
  */
 template<typename T>
-requires(is_integral<T>::value || is_floating_point<T>::value) int abs(T number)
+int
+abs(T number)
 {
     if (number < 0)
         return -number;
