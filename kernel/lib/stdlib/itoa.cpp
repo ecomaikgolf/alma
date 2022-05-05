50b360f2aac1c97c984e977d1cf1c655b7d5a992
Ernesto Martínez García <git@ecomaikgolf.com>
2022-05-05 18:26:07 +0200

    redundant code

--- kernel/lib/stdlib/itoa.cpp
+++ kernel/lib/stdlib/itoa.cpp
@@ -91,31 +91,3 @@ str(float num, char result[], int precision)
 {
     str(static_cast<double>(num), result, precision);
 }
-
-/**
- * Convert hex number to string (provided buffer)
- * @post Result is in result[] buffer parameter
- *
- * @param num Number to convert
- * @param result Provided buffer and result
- */
-template<typename T>
-void
-hstr(T num, char result[])
-{
-    /** Pointer at the end of the number */
-    unsigned char *ptr = reinterpret_cast<unsigned char *>(&num + 1);
-    ptr--;
-
-    /** Iterate over the bytes */
-    for (unsigned long i = 0; i < sizeof(T); i++) {
-        /** 1 byte = 2 hex values */
-        int lval          = (*ptr & 0xF0) >> 4;
-        int rval          = (*ptr & 0x0F);
-        result[2 * i]     = lval + (lval > 9 ? '7' : '0');
-        result[2 * i + 1] = rval + (rval > 9 ? '7' : '0');
-        ptr--;
-    }
-
-    result[2 * sizeof(T)] = '\0';
-}
